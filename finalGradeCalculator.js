f3ab35793a290bffcbe39db0ace4509ef73c4aa1
ganginav <100102149+ganginav@users.noreply.github.com>
2024-02-03 15:13:50 -0500

    Update finalGradeCalculator.js

--- finalGradeCalculator.js
+++ finalGradeCalculator.js
@@ -1,9 +1,1 @@
-function calculateFinalGrade() {
-  var currentGrade = document.getElementById("currentGrade").value;
-  var desiredGrade = document.getElementById("desiredGrade").value;
-  var finalWorthPercentage = document.getElementById("finalWorthPercentage").value;
 
-  var finalGradeNeeded = ((desiredGrade/100) - (1 - finalWorthPercentage/100)*(currentGrade/100))/(finalWorthPercentage/100)*100;
-
-  document.getElementById("finalGrade").innerHTML = "Final grade needed: " + Math.round(finalGradeNeeded) + "%";
-}
